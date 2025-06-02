aa3beca0e56621dbf8ad2fe9091d00747d73e964
Ianis Catalin <73122512+IanisCatalin15@users.noreply.github.com>
2025-06-02 03:40:00 -0700

    Update hours.lua

--- vrp/client/hours.lua
+++ vrp/client/hours.lua
@@ -2,19 +2,18 @@
 -- MIT license (see LICENSE or vrp/vRPShared.lua)
 if not vRP.modules.hours then return end
 
-local hours = class("hours", vRP.Extension)
+local Hours = class("Hours", vRP.Extension)
 
 function hours:__construct()
     vRP.Extension.__construct(self)
 
     Citizen.CreateThread(function()
         while true do
-            Wait(15 * 60000) 
+            Wait(15 * 60000)
             print("Hours: 15 minutes have passed.")
-            self.remote._updateHours(0.25) 
+            self.remote._updateHours(0.25)
         end
     end)
 end
 
-
-vRP:registerExtension(hours)
+vRP:registerExtension(Hours)
